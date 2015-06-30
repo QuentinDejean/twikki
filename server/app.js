5ac60d7f61ecb3bb93ca89c76bab9f02c078462a
Quentin Dejean <quentin.dejean34@gmail.com>
2015-06-30 21:10:26 +1000

    Added back-end Twitter stream integration

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var twitter = require('./routes/twitter');
 
 var app = express();
@@ -26,7 +25,6 @@ app.use(express.static(path.join(__dirname, '../client')));
 //require('./auth/twitter/auth.twitter').setup();
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/twitter', twitter);
 
 // catch 404 and forward to error handler
