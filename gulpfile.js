ced3799c1b52c16aba899c9d96a2ad822cd70e4e
Quentin Dejean <quentin.dejean34@gmail.com>
2015-06-30 23:12:57 +1000

    Changed height of tweet Cleaned up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 'use strict';
 
 var gulp = require('gulp'),
-	gls = require('gulp-live-server'),
-	wiredep = require('wiredep').stream;
+	gls = require('gulp-live-server')
 
 var config = {
-	server: './bin/www',
-	index: './server/views/index.ejs'
+	server: './bin/www'
 };
 
 gulp.task('wiredep', function () {
@@ -22,10 +20,4 @@ gulp.task('serve', function () {
 	//1. run your script as a server
 	var server = gls.new(config.server);
 	server.start();
-
-	//use gulp.watch to trigger server actions(notify, start or stop)
-	//gulp.watch(['static/**/*.css', 'static/**/*.html'], function () {
-	//    server.notify.apply(server, arguments);
-	//});
-	//gulp.watch(config.server, server.start.bind(server)); //restart my server
 });
